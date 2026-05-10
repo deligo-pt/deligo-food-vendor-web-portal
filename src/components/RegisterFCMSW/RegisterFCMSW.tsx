87f946c9e45b54afde5c6df0dc634d0a900f12c0
MoRsHeD <mdmorshed0187@gmail.com>
2026-05-10 16:19:36 +0600

    updated updateFCMToken api and called it in registerFCMSW function provider

--- src/components/RegisterFCMSW/RegisterFCMSW.tsx
+++ src/components/RegisterFCMSW/RegisterFCMSW.tsx
@@ -1,19 +1,37 @@
 "use client";
 
+import { getAndSaveFcmToken } from "@/src/utils/fcmToken";
 import { useEffect } from "react";
+import Cookies from "js-cookie";
 
 export function RegisterFCMSW() {
   useEffect(() => {
     if (!("serviceWorker" in navigator)) return;
+    const accessToken = Cookies.get("accessToken");
 
-    navigator.serviceWorker.getRegistration("/").then((reg) => {
-      if (!reg) {
-        navigator.serviceWorker
-          .register("/firebase-messaging-sw.js")
-          .then(() => console.log("FCM SW registered"))
-          .catch(console.log);
+    const initializeFCM = async () => {
+      try {
+        // Get existing or register new SW
+        let reg = await navigator.serviceWorker.getRegistration("/");
+
+        if (!reg) {
+          reg = await navigator.serviceWorker.register("/firebase-messaging-sw.js");
+          console.log("FCM SW Registered");
+        }
+        // Wait for the service worker to be ready
+        await navigator.serviceWorker.ready;
+
+        //  Call our updated sync logic
+        if (accessToken) {
+          await getAndSaveFcmToken(accessToken);
+        }
+
+      } catch (error) {
+        console.error("FCM Initialization failed:", error);
       }
-    });
+    };
+
+    initializeFCM();
   }, []);
 
   return null;
